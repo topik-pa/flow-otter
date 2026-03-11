b303e64931858aa3cbb85e2a836e0015c533e3d8
Marco Pavan <marcopavan.mp@gmail.com>
2026-03-11 15:45:28 +0100

    Added stats in dashboard

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -5,6 +5,80 @@
  * @returns {Promise<void>} A promise that resolves when the initialization is complete.
  * @throws {Error} Silently catches and ignores errors when adoptedStyleSheets is not supported by the browser.
  */
+import {
+  sessionStorageKey,
+  updatedStoreEvent
+} from './../../scripts/globals.js'
+
+
+const updateStats = () => {
+  function countUniqueCrypto(transactions) {
+    const uniqueCryptos = new Set()
+    transactions.forEach(file => {
+      file.data.forEach(transaction => {
+      // Further processing can be done here if needed
+        if (transaction.baseAsset) {
+          uniqueCryptos.add(transaction.baseAsset)
+        }
+        if (transaction.quoteAsset) {
+          uniqueCryptos.add(transaction.quoteAsset)
+        }
+      })
+    })
+    return {
+      count: uniqueCryptos.size,
+      cryptos: Array.from(uniqueCryptos).sort()
+    }
+  }
+  function countUniqueExchanges(transactions) {
+    const uniqueExchanges = new Set()
+    transactions.forEach(file => {
+      // Further processing can be done here if needed
+      if (file.exchange) {
+        uniqueExchanges.add(file.exchange)
+      }
+    })
+    return {
+      count: uniqueExchanges.size,
+      exchanges: Array.from(uniqueExchanges).sort()
+    }
+  }
+
+  const storedTransactions = JSON.parse(sessionStorage.getItem(sessionStorageKey)) || []
+
+  const $filesCount = document.getElementById('files-count')
+  const $totalTransactions = document.getElementById('total-transactions')
+  const $cryptoCount = document.getElementById('crypto-count')
+  const $cryptoList = document.getElementById('crypto-list')
+  const $exchangesCount = document.getElementById('exchanges-count')
+  const $exchangesList = document.getElementById('exchanges-list')
+
+  if(storedTransactions.length === 0) {
+    $filesCount.innerText = 0
+    $totalTransactions.innerText = 0
+    $cryptoCount.innerText = 0
+    $cryptoList.innerText = '-'
+    $exchangesCount.innerText = 0
+    $exchangesList.innerText = '-'
+    return
+  }
+
+  const activeStoredTransactions = storedTransactions.filter(file => file.active)
+
+  
+  $filesCount.innerText = activeStoredTransactions.length
+  $totalTransactions.innerText = activeStoredTransactions.reduce(
+    (acc, file) => acc + (file.data ? file.data.length : 0), 0
+  )  
+
+  const cryptoStats = countUniqueCrypto(activeStoredTransactions)
+  $cryptoCount.innerText = cryptoStats.count
+  $cryptoList.innerText = cryptoStats.cryptos.join(', ')
+
+  const exchangeStats = countUniqueExchanges(activeStoredTransactions)
+  $exchangesCount.innerText = exchangeStats.count
+  $exchangesList.innerText = exchangeStats.exchanges.join(', ')
+}
 
 export default  {
   init: async() => {
@@ -17,5 +91,10 @@ export default  {
     } catch (err) { 
       // console.error('Error adopting style sheets:', err)
     }
+
+    updateStats()
+    window.addEventListener(updatedStoreEvent, () => {
+      updateStats()
+    })
   }
 }
